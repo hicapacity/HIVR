d63a1329f2efca01a9225ac9fc4e465af847da98
Jesse Thompson <chae.shi@gmail.com>
2014-02-24 15:46:53 -1000

    Added Breaking Donuts
    Donuts colliding with spheres explode into smaller pieces which derez
    after 10 seconds.

---
+++ Assets/Scripts/oncollideSpawn.js
@@ -0,0 +1,22 @@
+﻿
+ var projectile : Rigidbody;
+function OnCollisionEnter(myCol: Collision){
+ 
+
+
+
+
+    if(myCol.gameObject.name == "death_plane"){
+       Destroy(gameObject);
+       }
+     if(myCol.gameObject.name == "Sphere"){
+    		var clone : Rigidbody;
+			clone = Instantiate(projectile, transform.position, transform.rotation);
+			
+			// Give the cloned object an initial velocity along the current 
+			// object's Z axis
+			clone.velocity = transform.TransformDirection (Vector3.forward * 10);
+			   Destroy(gameObject);
+    }
+
+}
---
+++ Assets/Scripts/oncollide_destroy_timed.js
@@ -0,0 +1,17 @@
+﻿function OnCollisionEnter(myCol: Collision){
+ 
+    if(myCol.gameObject.name == "death_plane"){
+       Destroy(gameObject);
+       }
+
+
+}
+function Awake()
+{
+    Death();
+}
+function Death()
+{
+	  yield WaitForSeconds (10);
+  Destroy(gameObject);
+}
